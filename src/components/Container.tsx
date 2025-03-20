ed5ef629ec82b0b66bc685a558b9a33132aaf06a
cihankandis <cihankandis@gmail.com>
2025-03-20 23:31:31 +0100

    bars-4

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,9 +6,20 @@ const MAX_ACTIVE_BARS = 3;
 export const Container = () => {
   const [progressBars, setProgressBars] = useState<number[]>([]);
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [isPaused, setIsPaused] = useState<boolean>(false);
 
   const handleAdd = () => {
     setProgressBars((prev) => [...prev, prev.length]);
+  };
+
+  const handlePause = () => {
+    setIsPaused(!isPaused);
+  };
+
+  const handleReset = () => {
+    setActiveIndex(0);
+    setIsPaused(false);
+    setProgressBars([]);
   };
 
   return (
@@ -16,11 +27,18 @@ export const Container = () => {
       <button className="bg-gray-300 p-2 rounded" onClick={handleAdd}>
         Add
       </button>
+      <button className="bg-gray-300 p-2 rounded" onClick={handlePause}>
+        {isPaused ? "Resume" : "Pause"}
+      </button>
+      <button className="bg-gray-300 p-2 rounded" onClick={handleReset}>
+        Reset
+      </button>
 
       <div className="flex flex-col gap-2 mt-4">
         {progressBars.map((_, index) => (
           <ProgressBar
             key={index}
+            isPaused={isPaused}
             isActive={index < activeIndex + MAX_ACTIVE_BARS}
             onComplete={() => setActiveIndex(index + 1)}
           />
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,17 +1,26 @@
+import { clear } from "console";
 import { useEffect, useState } from "react";
 
 export const ProgressBar = ({
   isActive,
   onComplete,
+  isPaused,
 }: {
   isActive: boolean;
   onComplete: () => void;
+  isPaused: boolean;
 }) => {
   const [progress, setProgress] = useState(0);
+  const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    if (isActive && progress < 100) {
+    if (isPaused && timerId) {
+      clearInterval(timerId);
+      return;
+    }
+    if (isActive && !isPaused && progress < 100) {
       const interval = setInterval(() => {
+        setTimerId(interval);
         setProgress((prev) => {
           if (prev >= 100) {
             clearInterval(interval);
@@ -23,11 +32,10 @@ export const ProgressBar = ({
       }, 20);
       return () => clearInterval(interval);
     }
-  }, [isActive, onComplete]);
+  }, [isActive, onComplete, isPaused]);
 
   return (
     <div className="h-2 bg-gray-300 rounded relative">
-      isActive: {isActive ? "Yes" : "No"}
       <div
         className="h-2 bg-green-500 absolute rounded"
         style={{ width: `${progress}%` }}
